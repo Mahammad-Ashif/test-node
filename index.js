ef904ff6fa3c8934f9271aa8beec0a54a7a7dd67
Mahammad Ashif <ashif.mahammad@shyamfuture.com>
2026-07-07 17:27:43 +0530

    feat: index update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, This is your Node.js server running with Express!');
+  res.send('Hello, This is your Node.js server running with Express! V1');
 });
 
 app.listen(PORT, () => {
